b32e6886864f23853a3937aacaf294b1b8af34d2
Golam Rabby <golamrabbynwu@gmail.com>
2025-04-28 17:29:38 +0600

    file uploded

--- ADN_HCB_BACK/Controller/conveyanceController.js
+++ ADN_HCB_BACK/Controller/conveyanceController.js
@@ -24,3 +24,25 @@ export const getConveyance = async(req, res) =>{
     })
 }
 
+
+export const getPendingList = async(req, res) =>{
+    const pendingList = await Conveyance.find({
+        next_responsible_person_id : req.params.id
+    })
+
+    if (!pendingList) return res.status(404).json({ message: 'Pending List not found' });
+
+    res.status(200).json({
+        data: pendingList,
+        message:'Pending list found'
+    })
+}
+
+export const updateMultipleId = async(req, res)=>{
+    const multiID = req.body[0];
+    const {next_responsible_person, next_responsible_person_id} = req.body;
+    const {approver_name, approver_id, approver_designation} = req.body.approver;
+    console.log(multiID);
+    console.log(next_responsible_person , next_responsible_person_id);
+    console.log(approver_name, approver_id , approver_designation);
+}
--- ADN_HCB_BACK/Routers/conveyanceRouters.js
+++ ADN_HCB_BACK/Routers/conveyanceRouters.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { addConveyance, getConveyance } from '../Controller/conveyanceController.js';
+import { addConveyance, getConveyance, getPendingList, updateMultipleId } from '../Controller/conveyanceController.js';
 
 const conveyanceRouter = express.Router();
 
 conveyanceRouter.post('/addConveyance', addConveyance);
 conveyanceRouter.post('/getConveyance', getConveyance);
+conveyanceRouter.post('/updateMultipleId', updateMultipleId);
+
+conveyanceRouter.get('/pendingList/:id', getPendingList);
 
 export default conveyanceRouter;
