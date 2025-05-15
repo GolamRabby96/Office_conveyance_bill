758f271a0814e8e798a77f84fa3f4b0dd014a585
Golam Rabby <golamrabbynwu@gmail.com>
2025-05-15 17:45:20 +0600

    file updated

--- ADN_HCB_BACK/Model/zoneModel.js
+++ ADN_HCB_BACK/Model/zoneModel.js
@@ -1,13 +1,7 @@
 import { Schema, model } from "mongoose";
 
 const zoneSchema = new Schema({
-    zone_name:{
-        type : String
-    },
-    sub_zone:{
-        type:String
-    }
-
+ 
 },{timestamps : true})
 
 export default model('Zone', zoneSchema);
